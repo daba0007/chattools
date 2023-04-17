7d797ee6b41a4ba1b0a338a80e180474185fe228
Shuo Wang <shuo.wang@shopee.com>
2023-04-17 08:05:39 +0800

    fix: yield response

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-data = {"max_tokens": 2048, "top_p": 0.2, "temperature": 0.8, "mix":True, "messages": [{"role":"user", "content":"请问你知道 Inhouse 吗"}]}
+data = {"max_tokens": 2048, "top_p": 0.2, "temperature": 0.8, "mix":True, "messages": [{"role":"user", "content":"你好"}]}
 headers = {
   "Content-Type": "application/json",
   "Accept": "text/event-stream"
@@ -10,14 +10,18 @@ url = "https://xsre.workflow.sz.shopee.io/chat/completions"
 response = requests.post(url, headers=headers, json=data, stream=True)
 # Check if the request was successful
 if response.status_code == 200:
-  import re
   # Extract the generated text from the response
+  response_text = ""
+  previous_response = ''
   for line in response.iter_lines():
-    text = line.decode("utf-8")
-    json_strings = re.findall(r"\{[^}]*\}", text)
-    for json_string in json_strings:
-      data = json.loads(json_string)
-      response_text = data["response"]
-      print(response_text)
+    if line:
+      print(line)
+      data = json.loads(line.decode("utf-8"))
+      current_response = data['response']
+      if current_response == "[DONE]":
+        break
+      new_characters = current_response[len(previous_response):]
+      print(new_characters, end='', flush=True)
+      previous_response = current_response
 else:
     print(f"Request failed with status code {response.status_code}")
