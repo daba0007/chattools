a559d38c8e899ce5b25dac347df5a4445b18f210
Shuo Wang <shuo.wang@shopee.com>
2023-04-18 15:27:50 +0800

    feature: add langchain

--- utils/main.py
+++ utils/main.py
@@ -50,7 +50,7 @@ def setup_logger(app):
 def load_LLM():
     try:
         from importlib import import_module
-        utils.Model = import_module(f'plugins.llm_{utils.LLM_Type}').model
+        utils.Model = import_module(f"plugins.llm_{utils.LLM_Type}").model
     except Exception as e:
         print(f"LLM模型加载失败:{e}")
 
